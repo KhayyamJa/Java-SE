6dd3a25b7ac9644a625baf10ced63d4b5420e83d
KhayyamJa <k.jamaladdinov@gmail.com>
2023-09-18 12:51:24 +0400

    initial commit

---
+++ src/datetimeAPI/Task8.java
@@ -0,0 +1,12 @@
+package datetimeAPI;
+
+import java.time.LocalDate;
+
+public class Task8 {
+    public static void main(String[] args) {
+
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDate.now().plusWeeks(2));
+
+    }
+}
