0208c3aa7a71184235e89f0605bbd8ac8ae40cf1
KhayyamJa <k.jamaladdinov@gmail.com>
2023-09-24 22:17:01 +0400

    initial commit

---
+++ src/datetimeAPI/Task9.java
@@ -0,0 +1,9 @@
+package datetimeAPI;
+
+import java.time.LocalDate;
+
+public class Task9 {
+    public static void main(String[] args){
+        System.out.println(LocalDate.parse("2001-06-15"));
+    }
+}
